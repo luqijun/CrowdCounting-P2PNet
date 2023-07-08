be5895877a954eaf08239d583099968d7cd103cf
lqjun <1120948897@qq.com>
2023-07-08 23:05:03 +0800

    add train.sh

---
+++ train.sh
@@ -0,0 +1,12 @@
+CUDA_VISIBLE_DEVICES=0 python train.py --data_root /mnt/e/MyDocs/Code/Datasets/ShangHaiTech/ShanghaiTech_P2PNet \
+    --dataset_file SHHA \
+    --epochs 3500 \
+    --lr_drop 3500 \
+    --output_dir ./logs \
+    --checkpoints_dir ./weights \
+    --tensorboard_dir ./logs \
+    --lr 0.0001 \
+    --lr_backbone 0.00001 \
+    --batch_size 8 \
+    --eval_freq 1 \
+    --gpu_id 0
