02ba5e6f416025b09d0be98a3c58ddc3ba9211aa
lqjun <1120948897@qq.com>
2024-07-17 23:31:06 +0800

    添加ffnet模型

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,12 @@
 from .p2pnet import build
+from .ffnet.FFNet import build_ffnet
 
 # build the P2PNet model
 # set training to 'True' during training
 def build_model(args, training=False):
-    return build(args, training)
+    if args.arch == 'p2pnet':
+        return build(args, training)
+    elif args.arch == 'ffnet':
+        return build_ffnet(args, training)
+    else:
+        raise NotImplementedError(f'This model————{args.arch} has not been implemented yet.')
