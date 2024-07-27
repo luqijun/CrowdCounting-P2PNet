c3c93c6c3436ec0bd17e8a93956224cb9b523e00
lqjun <1120948897@qq.com>
2024-07-27 14:00:23 +0800

    添加mlp和位置编码代码

--- models/layers/__init__.py
+++ models/layers/__init__.py
@@ -1,3 +1,6 @@
 from .layer import *
 from .fpn import *
 from .segmentation_head import *
+from .deformable_detr.deformable_detr import build_deformable_detr
+from .position_encoding import build_position_encoding
+from .mlp import *
